a07e53f3dbb00dd657d47138d8799ed40845c91b
Lucas Tulio <lucasdnd@gmail.com>
2018-02-23 15:24:22 -0300

    chore(*): replace proptypes

--- src/DimensionComponent.js
+++ src/DimensionComponent.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
 import {View, Dimensions, PixelRatio, Platform} from 'react-native';
 import {calculate, asArray, splitClass, splitComma, toggle, WINDOW, window} from './componentHelpers';
 import listen from './listen';
